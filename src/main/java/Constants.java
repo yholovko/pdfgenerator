e2572b4dd75c862c281bd1a4b8211bbdfbd71491
alexander159 <fatality174@gmail.com>
2015-10-16 17:41:17 +0300

    replaced nextYPos with lastYPos replaced position values with more accurate moved values to constants file

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -5,4 +5,127 @@ public class Constants {
 
     public static final String WATKINS = "Watkins";
     public static final String NOURISH = "Nourish";
+
+
+    public static final float PARAGRAPH_SIZE = 11.33f;
+    public static final float PAGE_TOP_MARGIN = 42.51f;
+    public static final float PAGE_LEFT_MARGIN = 59.52f;
+    public static final float SECOND_COLUMN_LEFT_MARGIN = 418.39f; //cover image, isbn, price, etc.
+    public static final int ADVANCE_INFO_TITLE_ALLOWED_WIDTH = 248;
+    public static final int FIRST_COLUMN_ALLOWED_WIDTH = 351;
+    public static final int SECOND_COLUMN_ALLOWED_WIDTH = 147;
+
+    public static final float LOGO_WIDTH = 60.0f;
+    public static final float LOGO_HEIGHT = 60.0f;
+    public static final float COVER_WIDTH = 128.01f;
+
+    public static final float ADVANCE_INFO_TITLE_TOP_MARGIN = 56.97f;
+    public static final float ADVANCE_INFO_TITLE_LEFT_MARGIN = 162.70f;
+    public static final int ADVANCE_INFO_TITLE_FONT_SIZE = 18;
+    public static final float ADVANCE_INFO_TITLE_LINE_HEIGHT = ADVANCE_INFO_TITLE_FONT_SIZE * 1.2f;
+
+    public static final float TITLE_TOP_MARGIN = 139.18f;
+    public static final int TITLE_FONT_SIZE = 22;
+    public static final float TITLE_LINE_HEIGHT = TITLE_FONT_SIZE * 1.2f;
+
+    public static final float SUBTITLE_TOP_MARGIN = 21.25f;
+    public static final int SUBTITLE_FONT_SIZE = 13;
+    public static final float SUBTITLE_LINE_HEIGHT = SUBTITLE_FONT_SIZE * 1.5f;
+
+    public static final float AUTHOR_NAME_TOP_MARGIN = 26.07f;
+    public static final int AUTHOR_NAME_FONT_SIZE = 12;
+    public static final float AUTHOR_NAME_LINE_HEIGHT = AUTHOR_NAME_FONT_SIZE * 1.2f;
+
+    public static final float PROMO_HEADLINE_TOP_MARGIN = 23.24f;
+    public static final int PROMO_HEADLINE_FONT_SIZE = 9;
+    public static final float PROMO_HEADLINE_LINE_HEIGHT = PROMO_HEADLINE_FONT_SIZE * 1.6f;
+
+    public static final float SALES_HEADLINE_TOP_MARGIN = 22.96f;
+    public static final int SALES_HEADLINE_FONT_SIZE = 11;
+    public static final float SALES_HEADLINE_LINE_HEIGHT = SALES_HEADLINE_FONT_SIZE * 1.2f;
+
+    public static final float SALES_POINTS_TOP_MARGIN = 14.74f;
+    public static final int SALES_POINTS_FONT_SIZE = 9;
+    public static final float SALES_POINTS_LINE_HEIGHT = SALES_POINTS_TOP_MARGIN;
+
+    public static final float SYNOPSIS_HEADLINE_TOP_MARGIN = 24.09f;
+    public static final int SYNOPSIS_HEADLINE_FONT_SIZE = 11;
+    public static final float SYNOPSIS_HEADLINE_LINE_HEIGHT = SYNOPSIS_HEADLINE_FONT_SIZE * 1.2f;
+
+    public static final float SYNOPSIS_TOP_MARGIN = 14.74f;
+    public static final int SYNOPSIS_FONT_SIZE = 9;
+    public static final float SYNOPSIS_LINE_HEIGHT = SYNOPSIS_TOP_MARGIN;
+
+    public static final float COMPANY_INFO_LEFT_MARGIN = 85.03f;
+    public static final float COMPANY_INFO_BOTTOM_MARGIN = 792.47f;
+    public static final int COMPANY_INFO_FONT_SIZE = 8;
+    public static final float COMPANY_INFO_LINE_HEIGHT = COMPANY_INFO_FONT_SIZE * 1.2f;
+
+    public static final float ISBN_TOP_MARGIN = 268.15f;
+    public static final int ISBN_FONT_SIZE = 9;
+    public static final float ISBN_LINE_HEIGHT = ISBN_FONT_SIZE * 1.2f;
+
+    public static final float PUBLISHED_DATE_TOP_MARGIN = 11.62f;
+    public static final int PUBLISHED_DATE_FONT_SIZE = 8;
+    public static final float PUBLISHED_DATE_LINE_HEIGHT = PUBLISHED_DATE_FONT_SIZE * 1.2f;
+
+    public static final float PRICE_TOP_MARGIN = 11.62f;
+    public static final int PRICE_FONT_SIZE = 8;
+    public static final float PRICE_LINE_HEIGHT = PRICE_FONT_SIZE * 1.2f;
+
+    public static final float CATEGORY_HEADLINE_TOP_MARGIN = 16.44f;
+    public static final int CATEGORY_HEADLINE_FONT_SIZE = 9;
+    public static final float CATEGORY_HEADLINE_LINE_HEIGHT = CATEGORY_HEADLINE_FONT_SIZE * 1.2f;
+
+    public static final float CATEGORY_TOP_MARGIN = 11.62f;
+    public static final int CATEGORY_FONT_SIZE = 8;
+    public static final float CATEGORY_LINE_HEIGHT = CATEGORY_TOP_MARGIN;
+
+    public static final float BINDING_HEADLINE_TOP_MARGIN = 16.44f;
+    public static final int BINDING_HEADLINE_FONT_SIZE = 9;
+    public static final float BINDING_HEADLINE_LINE_HEIGHT = BINDING_HEADLINE_FONT_SIZE * 1.2f;
+
+    public static final float BINDING_TOP_MARGIN = 11.62f;
+    public static final int BINDING__FONT_SIZE = 8;
+    public static final float BINDING_LINE_HEIGHT = BINDING_TOP_MARGIN;
+
+    public static final float FORMAT_HEADLINE_TOP_MARGIN = 16.44f;
+    public static final int FORMAT_HEADLINE_FONT_SIZE = 9;
+    public static final float FORMAT_HEADLINE_LINE_HEIGHT = FORMAT_HEADLINE_FONT_SIZE * 1.2f;
+
+    public static final float FORMAT_TOP_MARGIN = 11.62f;
+    public static final int FORMAT_FONT_SIZE = 8;
+    public static final float FORMAT_LINE_HEIGHT = FORMAT_TOP_MARGIN;
+
+    public static final float EXTENT_HEADLINE_TOP_MARGIN = 16.44f;
+    public static final int EXTENT_HEADLINE_FONT_SIZE = 9;
+    public static final float EXTENT_HEADLINE_LINE_HEIGHT = EXTENT_HEADLINE_FONT_SIZE * 1.2f;
+
+    public static final float EXTENT_TOP_MARGIN = 11.62f;
+    public static final int EXTENT_FONT_SIZE = 8;
+    public static final float EXTENT_LINE_HEIGHT = EXTENT_TOP_MARGIN;
+
+    public static final float ILLUSTRATIONS_HEADLINE_TOP_MARGIN = 16.44f;
+    public static final int ILLUSTRATIONS_HEADLINE_FONT_SIZE = 9;
+    public static final float ILLUSTRATIONS_HEADLINE_LINE_HEIGHT = ILLUSTRATIONS_HEADLINE_FONT_SIZE * 1.2f;
+
+    public static final float ILLUSTRATIONS_TOP_MARGIN = 11.62f;
+    public static final int ILLUSTRATIONS_FONT_SIZE = 8;
+    public static final float ILLUSTRATIONS_LINE_HEIGHT = ILLUSTRATIONS_TOP_MARGIN;
+
+    public static final float AUTHOR_LOCATION_HEADLINE_TOP_MARGIN = 16.44f;
+    public static final int AUTHOR_LOCATION_HEADLINE_FONT_SIZE = 9;
+    public static final float AUTHOR_LOCATION_HEADLINE_LINE_HEIGHT = AUTHOR_LOCATION_HEADLINE_FONT_SIZE * 1.2f;
+
+    public static final float AUTHOR_LOCATION_TOP_MARGIN = 11.62f;
+    public static final int AUTHOR_LOCATION_FONT_SIZE = 8;
+    public static final float AUTHOR_LOCATION_LINE_HEIGHT = AUTHOR_LOCATION_TOP_MARGIN;
+
+    public static final float ABOUT_AUTHOR_HEADLINE_TOP_MARGIN = 16.44f;
+    public static final int ABOUT_AUTHOR_HEADLINE_FONT_SIZE = 9;
+    public static final float ABOUT_AUTHOR_HEADLINE_LINE_HEIGHT = ABOUT_AUTHOR_HEADLINE_FONT_SIZE * 1.2f;
+
+    public static final float ABOUT_AUTHOR_TOP_MARGIN = 11.62f;
+    public static final int ABOUT_AUTHOR_FONT_SIZE = 8;
+    public static final float ABOUT_AUTHOR_LINE_HEIGHT = ABOUT_AUTHOR_TOP_MARGIN;
 }
--- src/main/java/PDF.java
+++ src/main/java/PDF.java
@@ -28,7 +28,7 @@ public class PDF {
     private Color primaryColor;
     private Color secondaryColor;
 
-    private float nextYPos;         //equals lastY + lineHeight
+    private float lastYPos;         //equals lastY
 
     /**
      * @param documentTitle  Nourish or Watkins
@@ -69,7 +69,7 @@ public class PDF {
             } else {
                 logoImg = PDImageXObject.createFromFile(new File(Constants.FILE_SAVING_DIR + Constants.LOGO_FILE_NAME + FileDownloader.getFileExtension(info.getLogo())), document);
             }
-            contentStream.drawImage(logoImg, 59.52f, rect.getHeight() - 60.00f - 42.51f, 60.00f, 60.00f);
+            contentStream.drawImage(logoImg, Constants.PAGE_LEFT_MARGIN, rect.getHeight() - Constants.LOGO_HEIGHT - Constants.PAGE_TOP_MARGIN, Constants.LOGO_WIDTH, Constants.LOGO_HEIGHT);
         } catch (FileNotFoundException | NullPointerException | ImageReadException | IllegalArgumentException e) {
             e.printStackTrace();
         }
@@ -86,73 +86,69 @@ public class PDF {
                 coverImg = PDImageXObject.createFromFile(new File(Constants.FILE_SAVING_DIR + coverPictureFilename), document);
             }
 
-            float scale = coverImg.getWidth() / 128.01f;
+            float scale = coverImg.getWidth() / Constants.COVER_WIDTH;
             float height = coverImg.getHeight() / scale;
-            contentStream.drawImage(coverImg, 418.39f, rect.getHeight() - height - 42.51f, 128.01f, height);
+            contentStream.drawImage(coverImg, Constants.SECOND_COLUMN_LEFT_MARGIN, rect.getHeight() - height - Constants.PAGE_TOP_MARGIN, Constants.COVER_WIDTH, height);
         } catch (FileNotFoundException | NullPointerException | ImageReadException | IllegalArgumentException e) {
             e.printStackTrace();
         }
 
-        drawMultiLineText(documentTitle, 162.70f, rect.getHeight() - 56.97f, 248, contentStream, robotoLight, 18, primaryColor, 1.2f * 18, 2, false, false);
-        drawMultiLineText("Advance Information", 162.70f, rect.getHeight() - 56.97f - 1.2f * 18, 248, contentStream, robotoLight, 18, primaryColor, 1.2f * 18, 2, false, false);
-        drawMultiLineText(info.getTitle(), 59.52f, rect.getHeight() - 139.18f, 351, contentStream, robotoLight, 22, mainColor, 1.2f * 22, 0f, false, false);
-        drawMultiLineText(info.getSubtitle(), 59.52f, nextYPos + ((1.2f * 22 - 1.2f * 13) / 2), 351, contentStream, robotoLight, 13, primaryColor, 1.5f * 13, 0f, false, false);
-        drawMultiLineText(info.getAuthorName(), 59.52f, nextYPos - 0.5f * 13, 351, contentStream, robotoLight, 12, mainColor, 1.2f * 12, 0f, false, false);
-        drawMultiLineText(info.getPromotinalHeadline(), 59.52f, nextYPos - 0.6f * 13, 351, contentStream, robotoBold, 9, primaryColor, 1.6f * 9, 0f, false, false);
-        drawMultiLineText("SALES POINTS:", 59.52f, nextYPos + ((1.6f * 9) / 2) - 1.6f * 9 - 1, 351, contentStream, robotoBold, 11, primaryColor, 1.2f * 11, 0f, false, false);
+        drawMultiLineText(documentTitle, Constants.ADVANCE_INFO_TITLE_LEFT_MARGIN, rect.getHeight() - Constants.ADVANCE_INFO_TITLE_TOP_MARGIN, Constants.ADVANCE_INFO_TITLE_ALLOWED_WIDTH, contentStream, robotoLight, Constants.ADVANCE_INFO_TITLE_FONT_SIZE, primaryColor, Constants.ADVANCE_INFO_TITLE_LINE_HEIGHT, 2, false, false);
+        drawMultiLineText("Advance Information", Constants.ADVANCE_INFO_TITLE_LEFT_MARGIN, lastYPos - Constants.ADVANCE_INFO_TITLE_LINE_HEIGHT, Constants.ADVANCE_INFO_TITLE_ALLOWED_WIDTH, contentStream, robotoLight, Constants.ADVANCE_INFO_TITLE_FONT_SIZE, primaryColor, Constants.ADVANCE_INFO_TITLE_LINE_HEIGHT, 2, false, false);
+        drawMultiLineText(info.getTitle(), Constants.PAGE_LEFT_MARGIN, rect.getHeight() - Constants.TITLE_TOP_MARGIN, Constants.FIRST_COLUMN_ALLOWED_WIDTH, contentStream, robotoLight, Constants.TITLE_FONT_SIZE, mainColor, Constants.TITLE_LINE_HEIGHT, 0f, false, false);
+        drawMultiLineText(info.getSubtitle(), Constants.PAGE_LEFT_MARGIN, lastYPos - Constants.SUBTITLE_TOP_MARGIN, Constants.FIRST_COLUMN_ALLOWED_WIDTH, contentStream, robotoLight, Constants.SUBTITLE_FONT_SIZE, primaryColor, Constants.SUBTITLE_LINE_HEIGHT, 0f, false, false);
+        drawMultiLineText(info.getAuthorName(), Constants.PAGE_LEFT_MARGIN, lastYPos - Constants.AUTHOR_NAME_TOP_MARGIN, Constants.FIRST_COLUMN_ALLOWED_WIDTH, contentStream, robotoLight, Constants.AUTHOR_NAME_FONT_SIZE, mainColor, Constants.AUTHOR_NAME_LINE_HEIGHT, 0f, false, false);
+        drawMultiLineText(info.getPromotinalHeadline(), Constants.PAGE_LEFT_MARGIN, lastYPos - Constants.PROMO_HEADLINE_TOP_MARGIN, Constants.FIRST_COLUMN_ALLOWED_WIDTH, contentStream, robotoBold, Constants.PROMO_HEADLINE_FONT_SIZE, primaryColor, Constants.PROMO_HEADLINE_LINE_HEIGHT, 0f, false, false);
+        drawMultiLineText("SALES POINTS:", Constants.PAGE_LEFT_MARGIN, lastYPos - Constants.SALES_HEADLINE_TOP_MARGIN, Constants.FIRST_COLUMN_ALLOWED_WIDTH, contentStream, robotoBold, Constants.SALES_HEADLINE_FONT_SIZE, primaryColor, Constants.SALES_HEADLINE_LINE_HEIGHT, 0f, false, false);
 
-        for (int i = 0; i < info.getSalesPoints().size(); i++) {
-            if (i == 0) {
-                drawMultiLineText(info.getSalesPoints().get(i), 59.52f, nextYPos - 3, 351, contentStream, robotoLight, 9, secondaryColor, 1.6f * 9, 0f, false, false);
-            } else {
-                drawMultiLineText(info.getSalesPoints().get(i), 59.52f, nextYPos, 351, contentStream, robotoLight, 9, secondaryColor, 1.6f * 9, 0f, false, false);
-            }
+        for (String salesPoint : info.getSalesPoints()) {
+            drawMultiLineText(salesPoint, Constants.PAGE_LEFT_MARGIN, lastYPos - Constants.SALES_POINTS_TOP_MARGIN, Constants.FIRST_COLUMN_ALLOWED_WIDTH, contentStream, robotoLight, Constants.SALES_POINTS_FONT_SIZE, secondaryColor, Constants.SALES_POINTS_LINE_HEIGHT, 0f, false, false);
         }
 
-        drawMultiLineText("SYNOPSIS:", 59.52f, nextYPos + ((1.6f * 9) / 2) - 1.6f * 9 - 1, 351, contentStream, robotoBold, 11, primaryColor, 1.2f * 11, 0f, false, false);
+        drawMultiLineText("SYNOPSIS:", Constants.PAGE_LEFT_MARGIN, lastYPos - Constants.SYNOPSIS_HEADLINE_TOP_MARGIN, Constants.FIRST_COLUMN_ALLOWED_WIDTH, contentStream, robotoBold, Constants.SYNOPSIS_HEADLINE_FONT_SIZE, primaryColor, Constants.SYNOPSIS_HEADLINE_LINE_HEIGHT, 0f, false, false);
 
         String[] synopsis = info.getSynopsis().split("\n");    //split paragraphs
         for (int i = 0; i < synopsis.length; i++) {
-            if (i == 0) {
-                drawMultiLineText(synopsis[i], 59.52f, nextYPos - 3, 351 - 11, contentStream, robotoLight, 9, mainColor, 1.6f * 9, 0f, false, false); //1st line without paragraph
+            if (i == 0) {       //1st line without paragraph
+                drawMultiLineText(synopsis[i], Constants.PAGE_LEFT_MARGIN, lastYPos - Constants.SYNOPSIS_TOP_MARGIN, Constants.FIRST_COLUMN_ALLOWED_WIDTH, contentStream, robotoLight, Constants.SYNOPSIS_FONT_SIZE, mainColor, Constants.SYNOPSIS_LINE_HEIGHT, 0f, false, false);
             } else {
-                drawMultiLineText(synopsis[i], 59.52f, nextYPos, 351 - 11, contentStream, robotoLight, 9, mainColor, 1.6f * 9, 0f, true, false);
+                drawMultiLineText(synopsis[i], Constants.PAGE_LEFT_MARGIN, lastYPos - Constants.SYNOPSIS_TOP_MARGIN, Constants.FIRST_COLUMN_ALLOWED_WIDTH - (int)Constants.PARAGRAPH_SIZE, contentStream, robotoLight, Constants.SYNOPSIS_FONT_SIZE, mainColor, Constants.SYNOPSIS_LINE_HEIGHT, 0f, true, false);
             }
         }
 
-        drawMultiLineText(String.format("© %s enquiries@watkinspublishing.com", documentTitle), 85.03f, rect.getHeight() - 792.47f, (int) rect.getWidth(), contentStream, robotoLight, 8, mainColor, 1.2f * 8, 0f, false, false);
-        drawMultiLineText("Angel Business Club 359 Goswell Rd London  EC1V 7JL Phone: 0207 323 2229 AI generated by Bibliocloud on 12 October 2015", 85.03f, nextYPos, (int) rect.getWidth(), contentStream, robotoLight, 8, mainColor, 1.2f * 8, 0f, false, false);
-        drawMultiLineText("ISBN: " + info.getIsbn(), 418.39f, rect.getHeight() - 268.15f, 147, contentStream, robotoRegular, 9, secondaryColor, 1.2f * 9, 0f, false, true);
-        drawMultiLineText("Published: " + simpleDateFormat.format(info.getPublicationDate()).toUpperCase(), 418.39f, nextYPos, 147, contentStream, robotoRegular, 8, secondaryColor, 1.2f * 9, 0f, false, true);
-        drawMultiLineText("Price: £" + info.getPrice(), 418.39f, nextYPos, 147, contentStream, robotoRegular, 8, secondaryColor, 1.2f * 9, 0f, false, true);
-        drawMultiLineText("CATEGORY", 418.39f, nextYPos - 7.3f, 147, contentStream, robotoBold, 9, primaryColor, 1.2f * 9, 0f, false, true);
+        drawMultiLineText(String.format("© %s enquiries@watkinspublishing.com", documentTitle), Constants.COMPANY_INFO_LEFT_MARGIN, rect.getHeight() - Constants.COMPANY_INFO_BOTTOM_MARGIN, (int) rect.getWidth(), contentStream, robotoLight, Constants.COMPANY_INFO_FONT_SIZE, mainColor, Constants.COMPANY_INFO_LINE_HEIGHT, 0f, false, false);
+        drawMultiLineText("Angel Business Club 359 Goswell Rd London  EC1V 7JL Phone: 0207 323 2229 AI generated by Bibliocloud on 12 October 2015", Constants.COMPANY_INFO_LEFT_MARGIN, lastYPos - Constants.COMPANY_INFO_LINE_HEIGHT, (int) rect.getWidth(), contentStream, robotoLight, Constants.COMPANY_INFO_FONT_SIZE, mainColor, Constants.COMPANY_INFO_LINE_HEIGHT, 0f, false, false);
+        drawMultiLineText("ISBN: " + info.getIsbn(), Constants.SECOND_COLUMN_LEFT_MARGIN, rect.getHeight() - Constants.ISBN_TOP_MARGIN, Constants.SECOND_COLUMN_ALLOWED_WIDTH, contentStream, robotoRegular, Constants.ISBN_FONT_SIZE, secondaryColor, Constants.ISBN_LINE_HEIGHT, 0f, false, true);
+        drawMultiLineText("Published: " + simpleDateFormat.format(info.getPublicationDate()).toUpperCase(), Constants.SECOND_COLUMN_LEFT_MARGIN, lastYPos - Constants.PUBLISHED_DATE_TOP_MARGIN, Constants.SECOND_COLUMN_ALLOWED_WIDTH, contentStream, robotoRegular, Constants.PUBLISHED_DATE_FONT_SIZE, secondaryColor, Constants.PUBLISHED_DATE_LINE_HEIGHT, 0f, false, true);
+        drawMultiLineText("Price: £" + info.getPrice(), Constants.SECOND_COLUMN_LEFT_MARGIN, lastYPos - Constants.PRICE_TOP_MARGIN, Constants.SECOND_COLUMN_ALLOWED_WIDTH, contentStream, robotoRegular, Constants.PRICE_FONT_SIZE, secondaryColor, Constants.PRICE_LINE_HEIGHT, 0f, false, true);
+        drawMultiLineText("CATEGORY", Constants.SECOND_COLUMN_LEFT_MARGIN, lastYPos - Constants.CATEGORY_HEADLINE_TOP_MARGIN, Constants.SECOND_COLUMN_ALLOWED_WIDTH, contentStream, robotoBold, Constants.CATEGORY_HEADLINE_FONT_SIZE, primaryColor, Constants.CATEGORY_HEADLINE_LINE_HEIGHT, 0f, false, true);
 
         for (String catPart : info.getCategory().split("\n")) {
-            drawMultiLineText(catPart, 418.39f, nextYPos, 147, contentStream, robotoRegular, 8, secondaryColor, 1.2f * 9, 0f, false, true);
+            drawMultiLineText(catPart, Constants.SECOND_COLUMN_LEFT_MARGIN, lastYPos - Constants.CATEGORY_TOP_MARGIN, Constants.SECOND_COLUMN_ALLOWED_WIDTH, contentStream, robotoRegular, Constants.CATEGORY_FONT_SIZE, secondaryColor, Constants.CATEGORY_LINE_HEIGHT, 0f, false, true);
         }
 
-        drawMultiLineText("BINDING", 418.39f, nextYPos - 7.3f, 147, contentStream, robotoBold, 9, primaryColor, 1.2f * 9, 0f, false, true);
+        drawMultiLineText("BINDING", Constants.SECOND_COLUMN_LEFT_MARGIN, lastYPos - Constants.BINDING_HEADLINE_TOP_MARGIN, Constants.SECOND_COLUMN_ALLOWED_WIDTH, contentStream, robotoBold, Constants.BINDING_HEADLINE_FONT_SIZE, primaryColor, Constants.BINDING_HEADLINE_LINE_HEIGHT, 0f, false, true);
 
         for (String bindingPart : info.getBinding().split("\n")) {
-            drawMultiLineText(bindingPart, 418.39f, nextYPos, 147, contentStream, robotoRegular, 8, secondaryColor, 1.2f * 9, 0f, false, true);
+            drawMultiLineText(bindingPart, Constants.SECOND_COLUMN_LEFT_MARGIN, lastYPos - Constants.BINDING_TOP_MARGIN, Constants.SECOND_COLUMN_ALLOWED_WIDTH, contentStream, robotoRegular, Constants.BINDING__FONT_SIZE, secondaryColor, Constants.BINDING_LINE_HEIGHT, 0f, false, true);
         }
 
-        drawMultiLineText("FORMAT", 418.39f, nextYPos - 7.3f, 147, contentStream, robotoBold, 9, primaryColor, 1.2f * 9, 0f, false, true);
-        drawMultiLineText(info.getFormat(), 418.39f, nextYPos, 147, contentStream, robotoRegular, 8, secondaryColor, 1.2f * 9, 0f, false, true);
-        drawMultiLineText("EXTENT", 418.39f, nextYPos - 7.3f, 147, contentStream, robotoBold, 9, primaryColor, 1.2f * 9, 0f, false, true);
-        drawMultiLineText(info.getExtent(), 418.39f, nextYPos, 147, contentStream, robotoRegular, 8, secondaryColor, 1.2f * 9, 0f, false, true);
-        drawMultiLineText("ILLUSTRATIONS", 418.39f, nextYPos - 7.3f, 147, contentStream, robotoBold, 9, primaryColor, 1.2f * 9, 0f, false, true);
+        drawMultiLineText("FORMAT", Constants.SECOND_COLUMN_LEFT_MARGIN, lastYPos - Constants.FORMAT_HEADLINE_TOP_MARGIN, Constants.SECOND_COLUMN_ALLOWED_WIDTH, contentStream, robotoBold, Constants.FORMAT_HEADLINE_FONT_SIZE, primaryColor, Constants.FORMAT_HEADLINE_LINE_HEIGHT, 0f, false, true);
+        drawMultiLineText(info.getFormat(), Constants.SECOND_COLUMN_LEFT_MARGIN, lastYPos - Constants.FORMAT_TOP_MARGIN, Constants.SECOND_COLUMN_ALLOWED_WIDTH, contentStream, robotoRegular, Constants.FORMAT_FONT_SIZE, secondaryColor, Constants.FORMAT_LINE_HEIGHT, 0f, false, true);
+        drawMultiLineText("EXTENT", Constants.SECOND_COLUMN_LEFT_MARGIN, lastYPos - Constants.EXTENT_HEADLINE_TOP_MARGIN, Constants.SECOND_COLUMN_ALLOWED_WIDTH, contentStream, robotoBold, Constants.EXTENT_HEADLINE_FONT_SIZE, primaryColor, Constants.EXTENT_HEADLINE_LINE_HEIGHT, 0f, false, true);
+        drawMultiLineText(info.getExtent(), Constants.SECOND_COLUMN_LEFT_MARGIN, lastYPos - Constants.EXTENT_TOP_MARGIN, Constants.SECOND_COLUMN_ALLOWED_WIDTH, contentStream, robotoRegular, Constants.EXTENT_FONT_SIZE, secondaryColor, Constants.EXTENT_LINE_HEIGHT, 0f, false, true);
+        drawMultiLineText("ILLUSTRATIONS", Constants.SECOND_COLUMN_LEFT_MARGIN, lastYPos - Constants.ILLUSTRATIONS_HEADLINE_TOP_MARGIN, Constants.SECOND_COLUMN_ALLOWED_WIDTH, contentStream, robotoBold, Constants.ILLUSTRATIONS_HEADLINE_FONT_SIZE, primaryColor, Constants.ILLUSTRATIONS_HEADLINE_LINE_HEIGHT, 0f, false, true);
 
         for (String illustrationsPart : info.getIllustrations().split("\n")) {
-            drawMultiLineText(illustrationsPart, 418.39f, nextYPos, 147, contentStream, robotoRegular, 8, secondaryColor, 1.2f * 9, 0f, false, true);
+            drawMultiLineText(illustrationsPart, Constants.SECOND_COLUMN_LEFT_MARGIN, lastYPos - Constants.ILLUSTRATIONS_TOP_MARGIN, Constants.SECOND_COLUMN_ALLOWED_WIDTH, contentStream, robotoRegular, Constants.ILLUSTRATIONS_FONT_SIZE, secondaryColor, Constants.ILLUSTRATIONS_LINE_HEIGHT, 0f, false, true);
         }
 
-        drawMultiLineText("AUTHOR LOCATION", 418.39f, nextYPos - 7.3f, 147, contentStream, robotoBold, 9, primaryColor, 1.2f * 9, 0f, false, true);
-        drawMultiLineText(info.getAuthorLocation(), 418.39f, nextYPos, 147, contentStream, robotoRegular, 8, secondaryColor, 1.2f * 9, 0f, false, true);
-        drawMultiLineText("About the author:", 418.39f, nextYPos - 7.3f, 147, contentStream, robotoBold, 9, secondaryColor, 1.2f * 9, 0f, false, true);
+        drawMultiLineText("AUTHOR LOCATION", Constants.SECOND_COLUMN_LEFT_MARGIN, lastYPos - Constants.AUTHOR_LOCATION_HEADLINE_TOP_MARGIN, Constants.SECOND_COLUMN_ALLOWED_WIDTH, contentStream, robotoBold, Constants.AUTHOR_LOCATION_HEADLINE_FONT_SIZE, primaryColor, Constants.AUTHOR_LOCATION_HEADLINE_LINE_HEIGHT, 0f, false, true);
+        drawMultiLineText(info.getAuthorLocation(), Constants.SECOND_COLUMN_LEFT_MARGIN, lastYPos - Constants.AUTHOR_LOCATION_TOP_MARGIN, Constants.SECOND_COLUMN_ALLOWED_WIDTH, contentStream, robotoRegular, Constants.AUTHOR_LOCATION_FONT_SIZE, secondaryColor, Constants.AUTHOR_LOCATION_LINE_HEIGHT, 0f, false, true);
+        drawMultiLineText("About the author:", Constants.SECOND_COLUMN_LEFT_MARGIN, lastYPos - Constants.ABOUT_AUTHOR_HEADLINE_TOP_MARGIN, Constants.SECOND_COLUMN_ALLOWED_WIDTH, contentStream, robotoBold, Constants.ABOUT_AUTHOR_HEADLINE_FONT_SIZE, secondaryColor, Constants.ABOUT_AUTHOR_HEADLINE_LINE_HEIGHT, 0f, false, true);
 
         for (String aboutAuthorPart : info.getBio().split("\n")) {
-            drawMultiLineText(aboutAuthorPart, 418.39f, nextYPos, 147, contentStream, robotoRegular, 8, secondaryColor, 1.2f * 9, 0f, false, true);
+            drawMultiLineText(aboutAuthorPart, Constants.SECOND_COLUMN_LEFT_MARGIN, lastYPos - Constants.ABOUT_AUTHOR_TOP_MARGIN, Constants.SECOND_COLUMN_ALLOWED_WIDTH, contentStream, robotoRegular, Constants.ABOUT_AUTHOR_FONT_SIZE, secondaryColor, Constants.ABOUT_AUTHOR_LINE_HEIGHT, 0f, false, true);
         }
 
         contentStream.close();
@@ -200,7 +196,7 @@ public class PDF {
             // test the width of the current line + the current word
             int size;
             if (isFirstParagraph) {
-                size = (int) ((fontSize * font.getStringWidth(myLine + word) / 1000) + 11.33f);
+                size = (int) ((fontSize * font.getStringWidth(myLine + word) / 1000) + Constants.PARAGRAPH_SIZE);
                 isFirstParagraph = false;
             } else {
                 size = (int) (fontSize * font.getStringWidth(myLine + word) / 1000);
@@ -227,7 +223,7 @@ public class PDF {
             contentStream.setFont(font, fontSize);
             contentStream.setNonStrokingColor(fontColor);
             if (isFirstParagraph) {
-                contentStream.newLineAtOffset(x + 11.33f, y);
+                contentStream.newLineAtOffset(x + Constants.PARAGRAPH_SIZE, y);
                 isFirstParagraph = false;
             } else if (isRightAlignment) {
                 if (line.length() != 0 && line.charAt(line.length() - 1) == ' ') {   //delete last empty symbol (for better right alignment)
@@ -242,8 +238,8 @@ public class PDF {
 
             contentStream.showText(line);
             contentStream.endText();
+            lastYPos = y;
             y -= lineHeight;
-            nextYPos = y;
         }
 
         return lines.size();
